f9a8a307a8f591bbc7cb08c414b7a058780c222c
boltxyz <bolt8bp@gmail.com>
2018-10-01 14:13:01 +0400

    Done

--- src/structures/Cli.js
+++ src/structures/Cli.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
-const fs = require("fs");
-const child_process = require("child_process");
-const inquirer = require("inquirer");
-const Util = require("../util/Util.js");
+const fs = require('fs');
+const child_process = require('child_process');
+const inquirer = require('inquirer');
+const Util = require('../util/Util.js');
 
-const scopedPackagePattern = new RegExp("^(?:@([^/]+?)[/])?([^/]+?)$");
+const scopedPackagePattern = new RegExp('^(?:@([^/]+?)[/])?([^/]+?)$');
 const questions = [
   {
-    type: "input",
-    name: "name",
-    message: "Input a bot name..."
+    type: 'input',
+    name: 'name',
+    message: 'Input a bot name...'
   },
   {
-    type: "password",
-    name: "token",
-    message: "Input your Discord client token..."
+    type: 'password',
+    name: 'token',
+    message: 'Input your Discord client token...'
   }
 ];
 
@@ -47,27 +47,31 @@ inquirer.prompt(questions).then(answer => {
 });
 
 function _fetchPackage(dir) {
-  return `{
+  return
+  `{
 			"name": "${dir}",
 			"version": "0.0.1",
-			"description": "A Discord bot, generated using discordtools.",
+			"description": "A Discord bot, built using discordtools.",
 			"main": "index.js",
 			"author": "discordtools",
 			"dependencies": {
-				"discord.js": "^11.3.2"
+				"discord.js": "^11.4.2"
 			}
 		}`;
 }
 
 function _fetchScript(token, name) {
-  const tokenValue = token.length > 0 ? token : "client-token";
-  return `// ${name}, generated using discordtools.
-			const Discord = require('discord.js');
-			const client = new Discord.Client();
-			client.on('ready', () => {
-				console.log('Bot has started!');
-			});
-			client.login('${tokenValue}');`;
+  const tokenValue = token.length > 0 ? token : 'client-token';
+  return 
+  `// ${name}, built using discordtools.
+	const Discord = require('discord.js');
+	const client = new Discord.Client();
+
+	client.on('ready', () => {
+		console.log('Bot has started!');
+	});
+
+	client.login('${tokenValue}');`;
 }
 
 function validatePackage(name) {
@@ -75,83 +79,83 @@ function validatePackage(name) {
   var errors = [];
 
   if (name === null) {
-    errors.push("name cannot be null");
+    errors.push('Package name cannot be null');
     return finalize(warnings, errors);
   }
   if (name === undefined) {
-    errors.push("Package name cannot be undefined");
+    errors.push('Package name cannot be undefined');
     return finalize(warnings, errors);
   }
-  if (typeof name !== "string") {
-    errors.push("Package name must be a string");
+  if (typeof name !== 'string') {
+    errors.push('Package name must be a string');
     return finalize(warnings, errors);
   }
   if (!name.length) {
-    errors.push("Package name length must be greater than zero.");
+    errors.push('Package name length must be greater than zero.');
   }
   if (name.match(/^\./)) {
-    errors.push("Package name cannot start with a period.");
+    errors.push('Package name cannot start with a period.');
   }
   if (name.match(/^_/)) {
-    errors.push("Package name cannot start with an underscore.");
+    errors.push('Package name cannot start with an underscore.');
   }
   if (name.trim() !== name) {
-    errors.push("Package name cannot contain leading or trailing spaces.");
+    errors.push('Package name cannot contain leading or trailing spaces.');
   }
 
-  ["node_modules", "favicon.ico"].forEach(function(blacklistedName) {
+  ['node_modules', 'favicon.ico'].forEach(function(blacklistedName) {
     if (name.toLowerCase() === blacklistedName) {
-      errors.push(`${blacklistedName} is a blacklisted name.`);
+      errors.push(`${blacklistedName} is a blacklisted package name.`);
     }
   });
 
   var builtInModules = [
-    "assert",
-    "buffer",
-    "child_process",
-    "cluster",
-    "console",
-    "constants",
-    "crypto",
-    "dgram",
-    "dns",
-    "domain",
-    "events",
-    "fs",
-    "http",
-    "https",
-    "module",
-    "net",
-    "os",
-    "path",
-    "punycode",
-    "querystring",
-    "readline",
-    "repl",
-    "stream",
-    "string_decoder",
-    "sys",
-    "timers",
-    "tls",
-    "tty",
-    "url",
-    "util",
-    "vm",
-    "zlib"
+    'assert',
+    'buffer',
+    'child_process',
+    'cluster',
+    'console',
+    'constants',
+    'crypto',
+    'dgram',
+    'dns',
+    'domain',
+    'events',
+    'fs',
+    'http',
+    'https',
+    'module',
+    'net',
+    'os',
+    'path',
+    'punycode',
+    'querystring',
+    'readline',
+    'repl',
+    'stream',
+    'string_decoder',
+    'sys',
+    'timers',
+    'tls',
+    'tty',
+    'url',
+    'util',
+    'vm',
+    'zlib'
   ];
   builtInModules.forEach(function(builtin) {
     if (name.toLowerCase() === builtin) {
-      warnings.push(`${builtin} is a built-in module name.`);
+      warnings.push(`${builtin} is a built-in package name.`);
     }
   });
   if (name.length > 214) {
-    warnings.push("Package name cannot contain more than 214 characters");
+    warnings.push('Package name cannot contain more than 214 characters');
   }
   if (name.toLowerCase() !== name) {
-    warnings.push("Package name cannot contain uppercase characters.");
+    warnings.push('Package name cannot contain uppercase characters.');
   }
-  if (/[~'!()*]/.test(name.split("/").slice(-1)[0])) {
-    warnings.push('Package name cannot contain special characters ("~\'!()*")');
+  if (/[~'!()*]/.test(name.split('/').slice(-1)[0])) {
+    warnings.push('Package name cannot contain special characters ('~\'!()*')');
   }
 
   if (encodeURIComponent(name) !== name) {
@@ -166,7 +170,7 @@ function validatePackage(name) {
         return finalize(warnings, errors);
       }
     }
-    errors.push("Name can only contain URL-friendly characters.");
+    errors.push('Package name can only contain URL-friendly characters.');
   }
   return finalize(warnings, errors);
 }
