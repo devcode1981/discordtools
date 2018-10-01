a2f4f4ddd2f7eb67f6221218207f144da2b38990
boltxyz <bolt8bp@gmail.com>
2018-10-01 14:44:29 +0400

    Yeah, docs

---
+++ docs/scripts/main.js
@@ -0,0 +1,11 @@
+var array = [
+    'Welcome to discordtools documentation',
+    'discordtools documentation',
+    'discordtools docs',
+    'Welcome here.',
+    'discordtools',
+    'Started!'
+];
+
+var random = Math.floor(Math.random() * array.length);
+console.log(array[random]);
