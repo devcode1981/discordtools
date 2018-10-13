77f5d15c9573731854cdb5ab34e84753faa285d0
boltxyz <bolt8bp@gmail.com>
2018-10-13 13:29:51 +0400

    Done

--- src/structures/Cli.js
+++ src/structures/Cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const child_process = require('child_process');
-const commander = require('commander');
-const fs = require('fs');
-const inquirer = require('inquirer');
-const Constants = require('../util/Constants');
+const child_process = require("child_process");
+const commander = require("commander");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Constants = require("../util/Constants");
 
 function _fetchPackage(dir, author) {
   return;
@@ -37,76 +37,86 @@ function _validatePackage(name) {
   var errors = [];
 
   if (name === null) {
-    errors.push('Package name cannot be null');
+    errors.push("Package name cannot be null");
     return _finalize(warnings, errors);
   }
   if (name === undefined) {
-    errors.push('Package name cannot be undefined');
+    errors.push("Package name cannot be undefined");
     return _finalize(warnings, errors);
   }
-  if (typeof name !== 'string') {
-    errors.push('Package name must be a string');
+  if (typeof name !== "string") {
+    errors.push("Package name must be a string");
     return _finalize(warnings, errors);
   }
-  if (!name.length) errors.push('Package name length must be greater than zero.');
-  if (name.match(/^\./)) errors.push('Package name cannot start with a period.');
-  if (name.match(/^_/)) errors.push('Package name cannot start with an underscore.');
-  if (name.trim() !== name) errors.push('Package name cannot contain leading or trailing spaces.');
+  if (!name.length)
+    errors.push("Package name length must be greater than zero.");
+  if (name.match(/^\./))
+    errors.push("Package name cannot start with a period.");
+  if (name.match(/^_/))
+    errors.push("Package name cannot start with an underscore.");
+  if (name.trim() !== name)
+    errors.push("Package name cannot contain leading or trailing spaces.");
 
-  var blackListedModules = ['node_modules', 'favicon.ico'];
-  blackListedModules.forEach(function (blacklistedName) {
-    if (name.toLowerCase() === blacklistedName) errors.push(`${blacklistedName} is a blacklisted package name.`);
+  var blackListedModules = ["node_modules", "favicon.ico"];
+  blackListedModules.forEach(function(blacklistedName) {
+    if (name.toLowerCase() === blacklistedName)
+      errors.push(`${blacklistedName} is a blacklisted package name.`);
   });
 
   var builtInModules = [
-    'assert',
-    'buffer',
-    'child_process',
-    'cluster',
-    'console',
-    'constants',
-    'crypto',
-    'dgram',
-    'dns',
-    'domain',
-    'events',
-    'fs',
-    'http',
-    'https',
-    'module',
-    'net',
-    'os',
-    'path',
-    'punycode',
-    'querystring',
-    'readline',
-    'repl',
-    'stream',
-    'string_decoder',
-    'sys',
-    'timers',
-    'tls',
-    'tty',
-    'url',
-    'util',
-    'vm',
-    'zlib'
+    "assert",
+    "buffer",
+    "child_process",
+    "cluster",
+    "console",
+    "constants",
+    "crypto",
+    "dgram",
+    "dns",
+    "domain",
+    "events",
+    "fs",
+    "http",
+    "https",
+    "module",
+    "net",
+    "os",
+    "path",
+    "punycode",
+    "querystring",
+    "readline",
+    "repl",
+    "stream",
+    "string_decoder",
+    "sys",
+    "timers",
+    "tls",
+    "tty",
+    "url",
+    "util",
+    "vm",
+    "zlib"
   ];
-  builtInModules.forEach(function (module) {
-    if (name.toLowerCase() === module) warnings.push(`${module} is a built-in package name.`);
+  builtInModules.forEach(function(module) {
+    if (name.toLowerCase() === module)
+      warnings.push(`${module} is a built-in package name.`);
   });
 
-  if (name.length > 214) warnings.push('Package name cannot contain more than 214 characters');
-  if (name.toLowerCase() !== name) warnings.push('Package name cannot contain uppercase characters.');
-  if (/[~'!()*]/.test(name.split('/').slice(-1)[0])) warnings.push('Package name cannot contain special characters.');
+  if (name.length > 214)
+    warnings.push("Package name cannot contain more than 214 characters");
+  if (name.toLowerCase() !== name)
+    warnings.push("Package name cannot contain uppercase characters.");
+  if (/[~'!()*]/.test(name.split("/").slice(-1)[0]))
+    warnings.push("Package name cannot contain special characters.");
   if (encodeURIComponent(name) !== name) {
     var nameMatch = name.match(scopedPackagePattern);
     if (nameMatch) {
       var user = nameMatch[1];
       var pkg = nameMatch[2];
-      if (encodeURIComponent(user) === user && encodeURIComponent(pkg) === pkg) return _finalize(warnings, errors);
+      if (encodeURIComponent(user) === user && encodeURIComponent(pkg) === pkg)
+        return _finalize(warnings, errors);
     }
-    errors.push('Package name can only contain URL-friendly characters.');
+    errors.push("Package name can only contain URL-friendly characters.");
   }
   return _finalize(warnings, errors);
 }
@@ -123,30 +133,32 @@ function _finalize(warnings, errors) {
   return result;
 }
 
-commander.version(`${Constants.Package_JSON.version}`, '-v, --version')
-  .option('-d, --default, Return default options.')
-  .option('-a, --author, Add the author.')
+commander
+  .version(`${Constants.Package_JSON.version}`, "-v, --version")
+  .option("-d, --default, Return default options.")
+  .option("-a, --author, Add the author.")
   .parse(process.argv);
 
 if (!commander.default || !commander.author) {
-  const ques = [{
-      type: 'input',
-      name: 'name',
-      message: 'Input the bot name...'
+  const ques = [
+    {
+      type: "input",
+      name: "name",
+      message: "Input the bot name..."
     },
     {
-      type: 'input',
-      name: 'author',
-      message: 'Input the author name...'
+      type: "input",
+      name: "author",
+      message: "Input the author name..."
     },
     {
-      type: 'password',
-      name: 'token',
-      message: 'Input your Discord client token...'
+      type: "password",
+      name: "token",
+      message: "Input your Discord client token..."
     }
   ];
 
-  inquirer.prompt(ques).then((answer) => {
+  inquirer.prompt(ques).then(answer => {
     var validated = _validatePackage(answer.name);
 
     if (validated.errors !== undefined) {
@@ -162,83 +174,83 @@ if (!commander.default || !commander.author) {
 
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
-      console.log('Creating new folder...');
+      console.log("Creating new folder...");
       fs.writeFile(`${dir}/package.json`, _fetchPackage(dir, author), err => {
-        if (err) return console.error('Error while creating package.json.');
+        if (err) return console.error("Error while creating package.json.");
       });
 
-      console.log('Creating index.js...');
+      console.log("Creating index.js...");
       fs.writeFile(`${dir}/index.js`, _fetchScript(token, dir), err => {
-        if (err) return console.error('Error while creating index.js.');
+        if (err) return console.error("Error while creating index.js.");
       });
 
-      console.log('Installing dependencies...');
+      console.log("Installing dependencies...");
       child_process.exec(`cd ${dir} && npm install`, (err, stdout, stderr) => {
-        console.log('Finalizing... Done!');
+        console.log("Finalizing... Done!");
       });
     }
   });
 }
 
 if (commander.default) {
-  dir = 'discordtools-bot';
-  token = 'client-token';
-  author = 'discordtools';
+  dir = "discordtools-bot";
+  token = "client-token";
+  author = "discordtools";
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
-    console.log('Creating new folder...');
-    fs.writeFile(`${dir}/package.json`, _fetchPackage(dir, author), (err) => {
-      if (err) return console.error('Error while creating package.json.');
+    console.log("Creating new folder...");
+    fs.writeFile(`${dir}/package.json`, _fetchPackage(dir, author), err => {
+      if (err) return console.error("Error while creating package.json.");
     });
 
-    console.log('Creating index.js...');
+    console.log("Creating index.js...");
     fs.writeFile(`${dir}/index.js`, _fetchScript(token, dir), err => {
-      if (err) return console.error('Error while creating index.js.');
+      if (err) return console.error("Error while creating index.js.");
     });
 
-    console.log('Installing dependencies...');
+    console.log("Installing dependencies...");
     child_process.exec(`cd ${dir} && npm install`, (err, stdout, stderr) => {
       if (err) throw err;
-      console.log('Finalizing... Done!');
+      console.log("Finalizing... Done!");
     });
   }
 }
 
 if (commander.author) {
-  const questions = [{
-    type: 'name',
-    name: 'author',
-    message: 'Input the author name...'
-  }];
-
-  inquirer.prompt(questions).then((answer) => {
+  const questions = [
+    {
+      type: "name",
+      name: "author",
+      message: "Input the author name..."
+    }
+  ];
+  dir = "discordtools-bot";
+  token = "client-token";
+  inquirer.prompt(questions).then(answer => {
+    var validated = _validatePackage(dir);
     if (validated.errors !== undefined) {
-      validated.errors.forEach((err) => {
+      validated.errors.forEach(err => {
         console.log(err);
       });
       return;
     }
-
-    dir = 'discordtools-bot';
-    token = 'client-token';
     author = answer.author;
-
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
-      console.log('Creating new folder...');
-      fs.writeFile(`${dir}/package.json`, _fetchPackage(dir, author), (err) => {
-        if (err) return console.error('Error while creating package.json.');
+      console.log("Creating new folder...");
+      fs.writeFile(`${dir}/package.json`, _fetchPackage(dir, author), err => {
+        if (err) return console.error("Error while creating package.json.");
       });
 
-      console.log('Creating index.js...');
+      console.log("Creating index.js...");
       fs.writeFile(`${dir}/index.js`, _fetchScript(token, dir), err => {
-        if (err) return console.error('Error while creating index.js.');
+        if (err) return console.error("Error while creating index.js.");
       });
 
-      console.log('Installing dependencies...');
+      console.log("Installing dependencies...");
       child_process.exec(`cd ${dir} && npm install`, (err, stdout, stderr) => {
         if (err) throw err;
-        console.log('Finalizing... Done!');
+        console.log("Finalizing... Done!");
       });
     }
   });
